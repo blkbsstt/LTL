94af1c5e0b98451f5ef0c8d176b543bd66963558
Blake Bassett <blkbsstt@gmail.com>
2015-03-10 21:38:02 -0500

    move to sbt dir structure

--- src/main/scala/blkbsstt/cs498/hw/ltl/LTL.scala
+++ src/main/scala/blkbsstt/cs498/hw/ltl/LTL.scala
@@ -4,8 +4,7 @@ object LTL extends App {
   val problem = Problem.SMT
   val formula = problem.formula
   val parser = new LTLParser()
-  val parsed = parser.parse(formula)
-  parsed match {
+  parser.parse(formula) match {
     case parser.Success(result, _) => LTLSat(result, 1, 3) match {
       case Some(model) => problem.display(model)
       case None        => println("Search Failed")
